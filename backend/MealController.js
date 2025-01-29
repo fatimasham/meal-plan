1e71fdb2b5f1d0ce1698498647fb71fc7fa36d59
“fatimasham” <diel1989@mail.ru>
2025-01-29 12:21:12 +0500

    first commit

---
+++ backend/MealController.js
@@ -0,0 +1,19 @@
+const MealModel = require('./MealModel');
+
+//GET
+
+module.exports.getMeal = async (req, res) => {
+    const myMeal = await MealModel.find();
+    res.send(myMeal)
+}
+
+//POST
+
+module.exports.saveMeals = async (req, res) => {
+    const { title } = req.body;
+    MealModel.create({ title }) 
+    .then((data) => { console.log('Meal added')
+    res.send(data)    
+})
+}
+
---
+++ backend/MealRoutes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { getMeal, saveMeals } = require('./MealController')
+
+const router = Router();
+
+router.get('/', getMeal);
+router.post('/saveMeals', saveMeals)
+
+
+module.exports = router;
